94cfa54cc5cf31c7d9f94cb81bdb087f23981cfe
小余君 <977540507@qq.com>
2021-06-05 15:49:13 +0800

    website

--- audio.js
+++ audio.js
@@ -36,10 +36,11 @@ $(function() {
 document.onreadystatechange = completeLoading;
 //加载状态为complete时移除loading效果
 function completeLoading() {
-if (document.readyState == "complete") {
-$("#loading").animate({
-"opacity": "0"
-}, 500).hide(1000);
+	if (document.readyState == "complete") {
+		$("#loading").animate({
+			"opacity": "0"
+		}, 500).hide(1000);
+	}
 }
-}
+
 
